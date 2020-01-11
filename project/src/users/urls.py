d4c7ace0a92c1aac32e400e3b95ca0c715f5348e
Miguel Jiménez <miguel1315@gmail.com>
2020-01-11 18:32:17 +0100

    Add POST request to create a commerce

--- project/src/users/urls.py
+++ project/src/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import CustomerCreateView
+from .views import CommerceCreateView, CustomerCreateView
+
 
 urlpatterns = [
     path('customer/', CustomerCreateView.as_view(), name='customer_create'),
+    path('commerce/', CommerceCreateView.as_view(), name='commerce_create'),
 ]
