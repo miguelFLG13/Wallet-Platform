6763ff4a1eb750c4da46a7efe230cedf138952e5
Miguel Jiménez <miguel1315@gmail.com>
2020-01-12 14:16:08 +0100

    Add GET wallet status request

--- project/src/wallets/urls.py
+++ project/src/wallets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import WalletCreateView
+from .views import WalletCreateView, WalletRetrieveView
 
 
 urlpatterns = [
     path('', WalletCreateView.as_view(), name='wallet_create'),
+    path('<uuid:uuid>/', WalletRetrieveView.as_view(), name='wallet'),
 ]
